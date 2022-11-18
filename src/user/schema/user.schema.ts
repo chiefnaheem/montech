7d6ed5d731e7c6d267b0db21d58a21c37bccfb02
naheem adedokun <nadedokun@heckerbella.com>
2022-11-18 16:19:15 +0100

    user models

---
+++ src/user/schema/user.schema.ts
@@ -0,0 +1,65 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { InternalServerErrorException } from '@nestjs/common';
+import mongoose from 'mongoose';
+import * as bcrypt from 'bcrypt';
+import { Document, Types } from 'mongoose';
+
+@Schema({
+  timestamps: true,
+  toJSON: {
+    transform: (doc, ret) => {
+      delete ret.password;
+      delete ret.__v;
+      return ret;
+    },
+  },
+  autoIndex: true,
+  _id: true,
+})
+export class UserEntity {
+  @Prop()
+  firstName: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop()
+  lastName: string;
+
+  @Prop()
+  age: number;
+
+  @Prop()
+  gender: string;
+
+  static async isValidPassword(
+    password: string,
+    hash: string,
+  ): Promise<boolean> {
+    try {
+      const result = await bcrypt.compare(password, hash);
+      if (!result) {
+        return false;
+      }
+      return true;
+    } catch (e) {
+      throw new InternalServerErrorException(e.message);
+    }
+  }
+
+  static async hashPassword(pass: string): Promise<{ password; salt }> {
+    try {
+      const salt = await bcrypt.genSalt();
+      const password = await bcrypt.hash(pass, salt);
+      return { password, salt };
+    } catch (e) {
+      throw new InternalServerErrorException(e.message);
+    }
+  }
+}
+
+export const UserSchema = SchemaFactory.createForClass(UserEntity);
+export type UserDocument = UserEntity & Document;
